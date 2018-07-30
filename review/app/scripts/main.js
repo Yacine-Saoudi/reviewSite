ed9572a5e10d4437e7bd2510288ed662b6dfb106
1801-AD-7NH36M2\Outreach20 <yacine.abdelhamid.saoudi@gmail.com>
2018-07-30 15:46:06 -0400

    user comment output code

--- review/app/scripts/main.js
+++ review/app/scripts/main.js
@@ -1,7 +1,29 @@
-var xa;
+var useInput;
+var locArray=["commOne","commTwo","commThree","commFour"];
+var commId=0
+var scoreArray=[]
+var nameArray=[]
+var commentArray
+
 $("#submit").click(function()
 {
-xa=document.getElementById("#reviewScore") ;
-("commOne")=xn.elements[].value;
+	debugger;
+
+	var useScore, useName, useComment
+
+	useScore=document.getElementById("score") ;
+	scoreArray[commId=useScore.elements["reviewScore"].value;
+	document.getElementById(commentArray[commId]).innerHTML=locArray[commId]
+
+	useName=document.getElementById("name");
+	nameArray[commId]=useName.elements["reviewName"]
+	document.getElementById(commentArray[commId]).innerHTML=locArray[commId]
+
+	useComment=document.getElementById("comment");
+	commentArray[commId]=useName.elements["userReview"]
+	document.getElementById(commentArray[commId]).innerHTML=locArray[commId]
+
+
+	commId++
 
 });
