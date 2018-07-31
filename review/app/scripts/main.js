08231232478dccd96da2853b8fa491dabaacc333
1801-AD-7NH36M2\Outreach20 <yacine.abdelhamid.saoudi@gmail.com>
2018-07-31 10:58:59 -0400

    adds a commenting function

--- review/app/scripts/main.js
+++ review/app/scripts/main.js
@@ -1,29 +1,51 @@
-var useInput;
-var locArray=["commOne","commTwo","commThree","commFour"];
+var avgArray=[];
 var commId=0
 var scoreArray=[]
 var nameArray=[]
-var commentArray
-
+var commentArray=[]
+var displayArray=[]
+var commPart=0;
 $("#submit").click(function()
+{
+	
+	$("#commOne").html(Comment())
+
+});
+
+function Comment ()
 {
 	debugger;
 
-	var useScore, useName, useComment
-
-	useScore=document.getElementById("score") ;
-	scoreArray[commId=useScore.elements["reviewScore"].value;
-	document.getElementById(commentArray[commId]).innerHTML=locArray[commId]
+	var useScore=" ", useName=" ", useComment=" ";
 
 	useName=document.getElementById("name");
-	nameArray[commId]=useName.elements["reviewName"]
-	document.getElementById(commentArray[commId]).innerHTML=locArray[commId]
+	nameArray[commId]=useName.elements["userName"].value;
+	displayArray[0]=nameArray[commPart]
+
+	useScore=document.getElementById("score");
+	scoreArray[commId]=useScore.elements["reviewScore"].value;
+	displayArray[1]=scoreArray[commPart]
+
+
 
 	useComment=document.getElementById("comment");
-	commentArray[commId]=useName.elements["userReview"]
-	document.getElementById(commentArray[commId]).innerHTML=locArray[commId]
+	commentArray[commId]=useComment.elements["userReview"].value;
+	displayArray[2]=commentArray[commPart]
+
+
+	commPart++
 
 
 	commId++
 
-});
+	for (var j = commPart-1; j < commPart; j++) {
+		avgArray[j]=displayArray[1]
+	}
+
+	for (var i = 0; i < displayArray.length; i++) {
+		$("#commOne").append(displayArray[i]+"<br><br>")
+	}
+		$("#commOne").append("<hr>")
+
+
+}
